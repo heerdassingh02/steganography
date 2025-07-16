230c6267b5b7313115bd375509b8a341f435f4c4
Heerdas Singh <heerdassingh@Heerdass-MacBook-Pro.local>
2025-07-16 23:21:43 +0530

    first commit

---
+++ EmbedMessage/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @heerdas
+ */
+public class Main {
+    public static void main(String arg[]) {
+    NewJFrame a=new NewJFrame();
+    a.setVisible(true);
+    a.setLocationRelativeTo(null);
+    }
+}
